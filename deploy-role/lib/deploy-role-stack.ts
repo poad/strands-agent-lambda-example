ae4ffcf1604aa13a48672dc3c05355907769a6f8
Kenji Saito <ken-yo@mbr.nifty.com>
2026-08-04 20:28:44 +0900

    新しい GitHub OIDC トークンの形式に対応

--- deploy-role/lib/deploy-role-stack.ts
+++ deploy-role/lib/deploy-role-stack.ts
@@ -6,19 +6,27 @@ export class DeployRoleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const repositoryId = this.node.tryGetContext('repository-id');
+    const repositoryOwnerId = this.node.tryGetContext('repository-owner-id');
+
     const cdkDeployPolicy = iam.ManagedPolicy.fromManagedPolicyName(this, 'CdkDeployPolicy', 'CdkDeployMinimalPolicy');
 
     new iam.Role(this, 'Role', {
       roleName: 'strands-agent-lambda-example-deploy-role',
       assumedBy: new iam.FederatedPrincipal(
         `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`, {
-          StringLike: {
-            'token.actions.githubusercontent.com:sub': 'repo:poad/strands-agent-lambda-example:*',
-          },
-          StringEquals: {
-            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
-          },
+        StringEquals: {
+          'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+          "token.actions.githubusercontent.com:repository_id": repositoryId,
+          "token.actions.githubusercontent.com:repository_owner_id": repositoryOwnerId,
         },
+        StringLike: {
+          'token.actions.githubusercontent.com:sub': [
+            'repo:poad/*',
+            'repo:poad@*',
+          ],
+        },
+      },
         'sts:AssumeRoleWithWebIdentity',
       ).withSessionTags(),
       managedPolicies: [
